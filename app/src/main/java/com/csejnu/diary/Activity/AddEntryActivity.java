cf0cb5acbce5beaeeb631c8ee95941e56cd8f2c0
Jahangir <jahangirhossainm69@gmail.com>
2020-12-27 13:20:41 +0600

    Initial commit

---
+++ app/src/main/java/com/csejnu/diary/Activity/AddEntryActivity.java
@@ -0,0 +1,1 @@
+package com.csejnu.diary.Activity;import androidx.appcompat.app.AppCompatActivity;import android.os.Bundle;import com.csejnu.diary.R;public class AddEntryActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_add_entry);    }}
